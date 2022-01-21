69f062b88ee06b181aa149165b0af8036519abed
Nerry Jean Francois <nerryjf80@gmail.com>
2022-01-21 19:17:24 -0300

    Category Repository created in memory

--- src/main/java/com/nerryjf/meu/primeiro/projeto/MeuPrimeiroProjetoApplication.java
+++ src/main/java/com/nerryjf/meu/primeiro/projeto/MeuPrimeiroProjetoApplication.java
@@ -1,13 +1,27 @@
 package com.nerryjf.meu.primeiro.projeto;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.nerryjf.meu.primeiro.projeto.entities.Category;
+import com.nerryjf.meu.primeiro.projeto.repositories.CategoryRepository;
+
 @SpringBootApplication
-public class MeuPrimeiroProjetoApplication {
+public class MeuPrimeiroProjetoApplication implements CommandLineRunner{
+	@Autowired
+	private CategoryRepository categoryRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(MeuPrimeiroProjetoApplication.class, args);
+	}
+	public void run(String...args) throws Exception{
+		Category cat1 = new Category(1L, "Electronics");
+		Category cat2 = new Category(2L, "Books");
+		
+		categoryRepository.save(cat1);
+		categoryRepository.save(cat2);
 	}
 
 }
--- src/main/java/com/nerryjf/meu/primeiro/projeto/resources/CategoryResource.java
+++ src/main/java/com/nerryjf/meu/primeiro/projeto/resources/CategoryResource.java
@@ -1,8 +1,8 @@
 package com.nerryjf.meu.primeiro.projeto.resources;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,22 +10,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.nerryjf.meu.primeiro.projeto.entities.Category;
+import com.nerryjf.meu.primeiro.projeto.repositories.CategoryRepository;
 
 @RestController
 @RequestMapping(value = "/categories")
 public class CategoryResource {
+	@Autowired
+	private CategoryRepository categoryRepository;
 
 	@GetMapping
 	public ResponseEntity<List<Category>> findAll() {
-		List<Category> list = new ArrayList<>();
-		list.add(new Category(1L, "Electronics"));
-		list.add(new Category(2L, "Books"));
+		List<Category> list = categoryRepository.findAll();
 		return ResponseEntity.ok().body(list);
 	}
 
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<Category> findById(@PathVariable Long id) {
-		Category cat = new Category(1L, "Electronics");
+		Category cat = categoryRepository.findById(id);
 		return ResponseEntity.ok().body(cat);
 	}
 
